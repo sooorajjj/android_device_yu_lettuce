f72bf90129723a628ebdcf5b192952926ad5de54
sooorajjj <sooorajjj@gmail.com>
2015-09-13 03:08:25 +0530

    lettuce: Build Metallium OS!!

--- metallium_lettuce.mk
+++ metallium_lettuce.mk
@@ -15,9 +15,9 @@
 $(call inherit-product, device/yu/lettuce/full_lettuce.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/metallium/main.mk)
 
-PRODUCT_NAME := cm_lettuce
+PRODUCT_NAME := metallium_lettuce
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := lettuce
 
